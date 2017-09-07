2146069e43af22b101d0215834ae029f4dd3e00a
Kalev Alpernas <kalev.alp@gmail.com>
2017-09-06 23:28:05 -0700

    Lambda shim - reading code from a js file + capture callbacks and only allow them if they're safe.

--- src/lambda-shim/lambda-shim.js
+++ src/lambda-shim/lambda-shim.js
@@ -1,32 +1,60 @@
 "use strict";
 
-// const fs = require('fs');
 import {NodeVM} from "vm2";
+import fs from "fs";
+import {PartialOrder} from "po-utils"
 
+const rmFilesInDir = function (dirPath) {
+    try {
+        const files = fs.readdirSync(dirPath);
 
-// rmFilesInDir = function(dirPath) {
-//     try { var files = fs.readdirSync(dirPath); }
-//     catch(e) { return; }
-//     if (files.length > 0)
-//         for (var i = 0; i < files.length; i++) {
-//             var filePath = dirPath + '/' + files[i];
-//             if (fs.statSync(filePath).isFile())
-//                 fs.unlinkSync(filePath);
-//             else
-//                 rmDir(filePath);
-//         }
-//     };
+        if (files.length > 0)
+            for (let i = 0; i < files.length; i++) {
+                const filePath = dirPath + '/' + files[i];
+                if (fs.statSync(filePath).isFile())
+                    fs.unlinkSync(filePath);
+                else
+                    rmFilesInDir(filePath);
+            }
+    }
+    catch(e) { }
+};
 
-// rmFilesInDir('/tmp')
+const notEmptyDir = function (dirPath) {
+    try {
+        const files = fs.readdirSync(dirPath);
+        return files.length > 0;
+    }
+    catch(e) { return true; }
+};
 
-module.exports.handler = function (externalEvent, externalContext, externalCallback) {
+const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
+const unsecuredLambda = fs.readFileSync(conf.unsecLambda, 'utf8');
+let label;
+
+let po;
+if (conf.usingPO) {
+    po = new PartialOrder(conf.labels);
+}
+
+module.exports.handler = function (event, context, callback) {
+
     const vm = new NodeVM({
         // console: 'off',
         console: 'inherit',
         sandbox: { 
-            externalEvent : externalEvent, 
-            externalContext : externalContext, 
-            externalCallback : externalCallback },
+            externalEvent : event,
+            externalContext : context,
+            externalCallback :
+                function (err, value) {
+                    if (po.lte(label, conf.securityBound)) {
+                        callback(err,value);
+                    } else {
+                        callback(null);
+                    }
+
+                }
+            },
         require: {
             external: true,
             builtin: [],
@@ -34,21 +62,14 @@ module.exports.handler = function (externalEvent, externalContext, externalCallb
         }
     });
 
-    vm.run(`
-        // Original lambda code.
-        //   Below is an example:
-        module.exports.handler = (event, context, callback) => {
-            console.log("Huh??");
-            console.log(event);
-            console.log(context);
-            console.log(callback);
-            // callback(null, 'Hello from Lambda');
-            callback();
-        };
+     let callres = vm.run(`
+/* ***********************************
+/* ** Original Lambda Code:
+${unsecuredLambda}
+/* ** End of Original Lambda Code:
+/* ***********************************
 
-        module.exports.callRes = module.exports.handler(externalEvent, externalContext, externalCallback);
-        delete module.exports.handler;
-
+module.exports = module.exports.handler(externalEvent, externalContext, externalCallback);
         `);
 };
 
