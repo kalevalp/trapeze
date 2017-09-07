ff49f1cc1fd98b1b212018a919722e039b8f54a1
Kalev Alpernas <kalev.alp@gmail.com>
2017-09-07 14:41:51 -0700

    Total Order utils.

---
+++ src/to-utils/to-utils.js
@@ -0,0 +1,58 @@
+class TotalOrder {
+    lte;
+
+    constructor(min, max, succ, lte) {
+        this.min = min;
+        this.max = max;
+        this.succ = succ;
+        if (lte !== undefined) {
+            this.lte = lte;
+        } else {
+            this.lte = function (val1,val2) {
+                return val1 <= val2;
+            }
+        }
+    }
+
+    getTop() {
+        return this.getMax();
+    }
+
+    getMax() {
+        return this.max;
+    }
+
+    getAllLE(label) {
+        let res = new Set();
+
+        if (this.succ !== undefined) {
+            let curr = this.min;
+            while (this.lte(curr,label)) {
+                res.add(curr);
+                curr = this.succ(curr);
+            }
+        } else if (
+            Number.isInteger(this.min) &&
+            Number.isInteger(this.max) &&
+            Number.isInteger(label))
+        {
+            for (let i = this.min; this.lte(i, label); i = i + 1) {
+                res.add(i);
+            }
+        } else {
+            throw "Error: trying to collect all less than or equal in a total ordering over non integers and without a successor function."
+        }
+        return res;
+    }
+}
+
+export {TotalOrder};
+
+
+
+/* ************************
+ *          Tests
+ * ************************ */
+if (process.argv[2] === "test") {
+
+}
