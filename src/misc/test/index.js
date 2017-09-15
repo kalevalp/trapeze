754e8c97630a9cdb7815e9f27f4e325e52ee6c1c
Kalev Alpernas <kalev.alp@gmail.com>
2017-09-15 13:46:15 -0700

    A 'scratch' lambda, used to test deployment and execution.

---
+++ src/misc/test/index.js
@@ -0,0 +1,15 @@
+fs = require("fs");
+
+exports.handler = (event, context, callback) => {
+    console.log("## File: " + __filename);
+    console.log("## Directory: " + __dirname);
+
+    let items = fs.readdirSync(".");
+    console.log(items);
+
+    for (let i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+    callback(null, 'Hello from Lambda');
+
+};
