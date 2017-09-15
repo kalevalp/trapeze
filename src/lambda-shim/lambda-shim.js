d9a8cb2a76944ab2fb282442a07f005e4c08d1c4
Kalev Alpernas <kalev.alp@gmail.com>
2017-09-15 16:51:53 -0700

    Made shim more generic + added key-value store mock class to sandbox.

--- src/lambda-shim/lambda-shim.js
+++ src/lambda-shim/lambda-shim.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 const PartialOrder = require("po-utils");
 const TotalOrder = require("to-utils");
 const auth = require("auth");
+const {SecureKV_PO} = require("secure-kv-po");
+const {SecureKV_TO} = require("secure-kv-to");
 
 const rmFilesInDir = function (dirPath) {
     try {
@@ -36,55 +38,83 @@ let label;
 
 const labelOrdering = conf.usingPO ? new PartialOrder(conf.labels) : new TotalOrder(conf.min, conf.max);
 
-module.exports.handler = function (event, context, callback) {
+const skv = conf.usingPO ?
+    new SecureKV_PO(conf.host, conf.user, conf.pass, labelOrdering) :
+    new SecureKV_TO(conf.host, conf.user, conf.pass);
 
-    if (notEmptyDir('/tmp/')) {
-        console.log("WARNING : /tmp/ dir not empty on fresh invocation of lambda. Might lead to data leak.")
-    }
+module.exports.makeShim = function (exp) {
 
-    // Run on behalf of invoking user.
-    label = auth(event.user, event.pass);
-    if (label === undefined) {
-        // In case getting the label failed, run on behalf of 'bottom' (completely unprivileged).
-        label = labelOrdering.getBottom();
-    }
+    exp.handler = function (event, context, callback) {
 
-    const vm = new NodeVM({
-        // console: 'off',
-        console: 'inherit',
-        sandbox: { 
-            externalEvent : event,
-            externalContext : context,
-            externalCallback :
-                function (err, value) {
-                    if (labelOrdering.lte(label, conf.securityBound)) {
-                        callback(err,value);
-                    } else {
-                        callback(null);
-                    }
-                },
-            bumpLabelTo :
-                function (newLabel) {
-                    if (labelOrdering.lte(label, newLabel)) {
-                        label = newLabel;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-            bumpLabelToTop :
-                function () {
-                    label = labelOrdering.getTop();
-                }
-            },
-        require: {
-            external: true,
-            builtin: [],
-            root: "./",
+        if (notEmptyDir('/tmp/')) {
+            console.log("WARNING : /tmp/ dir not empty on fresh invocation of lambda. Might lead to data leak.")
         }
-    });
 
-     vm.run(`
+        // Run on behalf of invoking user.
+        label = auth(event.user, event.pass);
+        if (label === undefined) {
+            // In case getting the label failed, run on behalf of 'bottom' (completely unprivileged).
+            label = labelOrdering.getBottom();
+        }
+
+        const vm = new NodeVM({
+            // console: 'off',
+            console: 'inherit',
+            sandbox: {
+                externalEvent: event,
+                externalContext: context,
+                externalCallback:
+                    function (err, value) {
+                        if (labelOrdering.lte(label, conf.securityBound)) {
+                            callback(err, value);
+                        } else {
+                            callback(null);
+                        }
+                    },
+                bumpLabelTo:
+                    function (newLabel) {
+                        if (labelOrdering.lte(label, newLabel)) {
+                            label = newLabel;
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    },
+                bumpLabelToTop:
+                    function () {
+                        label = labelOrdering.getTop();
+                    }
+            },
+            require: {
+                external: false,
+                builtin: ['fs'],
+                root: "./",
+                mock: {
+                    'kv-store' : {
+                        KV_Store : class {
+                            constructor(h, u, pwd) {}
+
+                            init(callback) {
+                                skv.init(callback);
+                            }
+
+                            close(callback) {
+                                skv.close(callback);
+                            }
+
+                            put (k, v, callback) {
+                                skv.put(k,v,label,callback);
+                            }
+                            get (k, callback) {
+                                skv.get(k,label,callback);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        vm.run(`
 /* ***********************************
 /* ** Original Lambda Code:
 ${unsecuredLambda}
@@ -93,4 +123,5 @@ ${unsecuredLambda}
 
 module.exports.${conf.handler}(externalEvent, externalContext, externalCallback);
         `);
+    };
 };
