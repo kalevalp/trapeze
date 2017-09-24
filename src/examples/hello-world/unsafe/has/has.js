14b06afb6c38198f366b1787837da17e6c40da8d
Kalev Alpernas <kalev.alp@gmail.com>
2017-09-24 14:13:42 -0700

    Has key function for the unsafe case.

---
+++ src/examples/hello-world/unsafe/has/has.js
@@ -0,0 +1,32 @@
+const {KV_Store} = require("kv-store");
+const fs = require("fs");
+
+const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
+
+module.exports.handler = function (event, context, callback) {
+    let kv = new KV_Store(conf.host, conf.user, conf.pass);
+
+    kv.init(function (err) {
+        if (err) {
+            callback(err);
+        } else {
+            kv.get(event.key, function (err, result) {
+                if (err) {
+                    callback(err);
+                } else {
+                    kv.close(function (err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            if (result === []) {
+                                callback(null, false);
+                            } else {
+                                callback(null, true);
+                            }
+                        }
+                    })
+                }
+            })
+        }
+    })
+};
