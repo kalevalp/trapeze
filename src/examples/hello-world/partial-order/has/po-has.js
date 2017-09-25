76dba9c688f648c68bd1ed88b950e173823cec3c
Kalev Alpernas <kalev.alp@gmail.com>
2017-09-24 19:13:19 -0700

    Removed testing code from the script.

--- src/examples/hello-world/partial-order/has/po-has.js
+++ src/examples/hello-world/partial-order/has/po-has.js
@@ -2,4 +2,4 @@ const shim = require("lambda-shim");
 
 shim.makeShim(module.exports);
 
-module.exports.handler({key: "A", user: "poguest", pass: "poguestpass"}, {}, (err, res) => {console.log("ERR: " + err + ";;; RES: " + res)});
+// module.exports.handler({key: "A", user: "poguest", pass: "poguestpass"}, {}, (err, res) => {console.log("ERR: " + err + ";;; RES: " + res)});
