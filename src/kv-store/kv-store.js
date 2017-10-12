c9025251ebfa7cbef02120cf5f3c9e4111ad810e
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-12 16:11:01 -0700

    Switched (unsafe) kv-store from callbacks to promises.

--- src/kv-store/kv-store.js
+++ src/kv-store/kv-store.js
@@ -1,13 +1,16 @@
 const mysql = require("mysql");
+const bbPromise = require('bluebird');
+
+// bbPromise.promisifyAll(mysql);
 
 class KV_Store {
     constructor(h, u, pwd, tbl) {
-        this.con = mysql.createConnection({
+        this.con = bbPromise.promisifyAll(mysql.createConnection({
             host: h,
             user: u,
             password: pwd,
             database: "unsecurekv"
-        });
+        }));
         if (tbl) {
             this.table = tbl;
         } else {
@@ -17,7 +20,7 @@ class KV_Store {
 
 
 
-    init(callback) {
+    init() {
         const showTablesSql = `
 SHOW TABLES like ?;
         `;
@@ -30,55 +33,42 @@ CREATE TABLE ${this.table} (
             `;
 
         console.log("** DEBUG: Call to init.");
-        this.con.connect((err) => {
-            if (err) {
-                console.log("** DEBUG: K-V store connection failed.");
-                callback(err);
-            } else {
-                console.log("** DEBUG: K-V store connected successfully.");
-                this.con.query(showTablesSql, [this.table], (err, result) => {
-                    if (err) {
-                        console.log("** DEBUG: Failed getting list of tables in database.");
-                        callback(err);
+        return this.con.connectAsync()
+            .then(
+                () => {
+                    console.log("** DEBUG: K-V store connected successfully.");
+                    return this.con.queryAsync(showTablesSql, [this.table]);
+                })
+            .then(
+                (result) => {
+                    console.log("** DEBUG: Query successful - getting list of tables in database.");
+                    if (result.length === 0) {
+                        console.log("** DEBUG: No table in database. Calling query to create table.");
+                        return this.con.queryAsync(createTableSql)
+                            .then(() => console.log("** DEBUG: Query successful - creating table."));
                     } else {
-                        console.log("** DEBUG: Query successful - getting list of tables in database.");
-                        if (result.length === 0) {
-                            console.log("** DEBUG: No table in database. Calling query to create table.");
-                            this.con.query(createTableSql, (err, result) => {
-                                if (err) {
-                                    console.log("** DEBUG: Failed creating table.");
-                                    callback(err);
-                                } else {
-                                    console.log("** DEBUG: Query successful - creating table.");
-                                    // console.log(result);
-                                    callback();
-                                }
-                            });
-                        } else {
-                            console.log("** DEBUG: table already exists in the database. No need to create it.");
-                            callback();
-                        }
+                        console.log("** DEBUG: table already exists in the database. No need to create it.");
                     }
-                });
-            }
-        });
-
+                })
+            .catch(
+                (err) => {
+                    console.log("*** DEBUG: Failed in init");
+                    return bbPromise.reject(err);
+                }
+            );
     }
 
-    close(callback) {
+    close() {
         console.log("** DEBUG: Call to close.");
-        this.con.end((err) => {
-            if (err) {
+        return this.con.endAsync()
+            .then(() => console.log("** DEBUG: K-V store connection closed successfully."))
+            .catch((err) => {
                 console.log("** DEBUG: Failed closing the database connection.");
-                callback(err);
-            } else {
-                console.log("** DEBUG: K-V store connection closed successfully.");
-                callback();
-            }
-        })
+                return bbPromise.reject(err);
+            });
     }
 
-    put (k, v, callback) {
+    put (k, v) {
         const putQuerySql = `
 INSERT INTO ${this.table} (rowkey,rowvalues) 
     VALUES (?, ?)
@@ -89,20 +79,20 @@ INSERT INTO ${this.table} (rowkey,rowvalues)
         console.log("** DEBUG:   Key:   " + k + ".");
         console.log("** DEBUG:   Value: " + v + ".");
 
-        this.con.query(putQuerySql,[k,v], (err, result) => {
-            if (err) {
-                console.log("** DEBUG: Query failed - inserting values.");
-                callback(err);
-            } else {
+        return this.con.queryAsync(putQuerySql,[k,v])
+            .then((result) => {
                 console.log("** DEBUG: Query successful - inserting values.");
                 console.log("** DEBUG: Query result:");
                 console.log(result);
                 console.log("** DEBUG: Query result />");
-                callback();
-            }
-        });
+            })
+            .catch( (err) => {
+                console.log("** DEBUG: Query failed - inserting values.");
+                return bbPromise.reject(err);
+            })
     }
-    get (k, callback) {
+
+    get (k) {
         const getQuerySql = `
 SELECT rowvalues 
 FROM ${this.table} 
@@ -111,46 +101,46 @@ WHERE rowkey = ?;
         console.log("** DEBUG: Call to get.");
         console.log("** DEBUG:   Key:   " + k + ".");
 
-        this.con.query(getQuerySql, [k], (err, result) => {
-            if (err) {
-                console.log("** DEBUG: Query failed - getting values.");
-                callback(err);
-            } else {
+        return this.con.queryAsync(getQuerySql, [k])
+            .then((result) => {
                 console.log("** DEBUG: Query successful - getting values.");
                 console.log("** DEBUG: Query result:");
                 console.log(result);
                 console.log("** DEBUG: Query result />");
 
-                if (result.length === 0) callback(null, "");
-                if (result.length === 1) callback(null, result[0]["rowvalues"]);
-                if (result.length > 1) callback("Inconsistent KeyValueStore");
-            }
-        });
+                if (result.length === 0) return "";
+                if (result.length === 1) return result[0]["rowvalues"];
+                if (result.length > 1) return bbPromise.reject("Inconsistent KeyValueStore");
+            })
+            .catch((err) => {
+                console.log("** DEBUG: Query failed - getting values.");
+                return bbPromise.reject(err);
+            });
     }
 
-    keys(callback) {
+    keys() {
         const keysQuerySql = `
 SELECT rowkey 
 FROM ${this.table};
     `;
         console.log("** DEBUG: Call to keys.");
 
-        this.con.query(keysQuerySql, (err, result) => {
-            if (err) {
-                console.log("** DEBUG: Query failed - getting keys.");
-                callback(err);
-            } else {
+        return this.con.queryAsync(keysQuerySql)
+            .then((result) => {
                 console.log("** DEBUG: Query successful - getting keys.");
                 console.log("** DEBUG: Query result:");
                 console.log(result);
                 console.log("** DEBUG: Query result />");
 
-                callback(null, result.map( x => x["rowkey"]));
-            }
-        });
+                return result.map(x => x["rowkey"]);
+            })
+            .catch((err) => {
+                console.log("** DEBUG: Query failed - getting keys.");
+                return bbPromise.reject(err);
+            });
     }
 
-    del(k, callback) {
+    del(k) {
         const deleteQuerySql = `
 DELETE FROM ${this.table}
 WHERE rowkey = ?;
@@ -158,46 +148,43 @@ WHERE rowkey = ?;
         console.log("** DEBUG: Call to delete.");
         console.log("** DEBUG:   Key:   " + k + ".");
 
-        this.con.query(deleteQuerySql, [k], (err, result) => {
-            if (err) {
-                console.log("** DEBUG: Query failed - deleting key.");
-                callback(err);
-            } else {
+        return this.con.queryAsync(deleteQuerySql, [k])
+            .then((result) => {
                 console.log("** DEBUG: Query successful - deleting key.");
                 console.log("** DEBUG: Query result:");
                 console.log(result);
                 console.log("** DEBUG: Query result />");
-
-                callback(null);
-            }
-        });
+            })
+            .catch((err) => {
+                console.log("** DEBUG: Query failed - deleting key.");
+                return bbPromise.reject(err);
+            });
     }
 
-    entries(callback) {
+    entries() {
         const entriesQuerySql = `
 SELECT rowkey, rowvalues 
 FROM ${this.table};
     `;
         console.log("** DEBUG: Call to entries.");
 
-        this.con.query(entriesQuerySql, (err, result) => {
-            if (err) {
-                console.log("** DEBUG: Query failed - getting entries.");
-                callback(err);
-            } else {
+        return this.con.queryAsync(entriesQuerySql)
+            .then((result) => {
                 console.log("** DEBUG: Query successful - getting entries.");
                 console.log("** DEBUG: Query result:");
                 console.log(result);
                 console.log("** DEBUG: Query result />");
 
-                callback(null, result.map( x => {
+                return result.map( x => {
                     return {
                         'key': x["rowkey"],
                         'val': x["rowvalues"],
-                    };
-                }));
-            }
-        });
+                    }})
+            })
+            .catch((err) => {
+                console.log("** DEBUG: Query failed - getting entries.");
+                return bbPromise.reject(err);
+            });
     }
 }
 
@@ -208,56 +195,82 @@ module.exports.KV_Store = KV_Store;
  * ************************ */
 
 if (process.argv[2] === "test") {
+    console.log(process.argv)
+
+
     const kv = new KV_Store(
         process.argv[3],
         process.argv[4],
         process.argv[5]
     );
-    console.log("************************");
-    console.log("Store Init Test:");
-    kv.init();
-    kv.con.query("SHOW TABLES;", function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    console.log("************************");
-    console.log("**");
-    console.log("************************");
-    console.log("Put Test:");
-    kv.con.query("SELECT * FROM kvstore;", function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    kv.put('a', 'value for a');
-    kv.con.query("SELECT * FROM kvstore;", function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    kv.put('b', 'a value for b');
-    kv.con.query("SELECT * FROM kvstore;", function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    kv.put('a', 'another value for a');
-    kv.con.query("SELECT * FROM kvstore;", function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    console.log("************************");
-    console.log("**");
-    console.log("************************");
-    console.log("Get Test:");
-    console.log("Stored value for key a : " + kv.get('a'));
-    console.log("Stored value for key b : " + kv.get('b'));
-    console.log("************************");
-    console.log("**");
-    console.log("************************");
-    console.log("Close Test:");
-    console.log("Connection state : " + kv.con.state);
-    kv.close();
-    console.log("Connection state : " + kv.con.state);
-    console.log("************************");
 
+    bbPromise.resolve()
+        .then(() => console.log("************************"))
+        .then(() => console.log("Store Init Test:"))
+        .then(() => kv.init())
+        .catch((err) => console.log("##### Init Test Error: " + err))
+        .then(() => console.log("************************"))
 
+        .then(() => console.log("**"))
 
+        .then(() => console.log("************************"))
+        .then(() => console.log("Put Test:"))
+        .then(() => kv.entries()).then((result) => console.log(result))
+        .then(() => kv.put('a', 'value for a'))
+        .then(() => kv.entries()).then((result) => console.log(result))
+        .then(() => kv.put('b', 'a value for b'))
+        .then(() => kv.entries()).then((result) => console.log(result))
+        .then(() => kv.put('a', 'another value for a'))
+        .then(() => kv.entries()).then((result) => console.log(result))
+        .catch((err) => console.log("##### Put Test Error: " + err))
+        .then(() => console.log("************************"))
+
+        .then(() => console.log("**"))
+
+        .then(() => console.log("************************"))
+        .then(() => console.log("Get Test:"))
+        .then(() => kv.get('a'))
+        .then((result) => console.log("Stored value for key a: " + result))
+        .then(() => kv.get('b'))
+        .then((result) => console.log("Stored value for key b: " + result))
+        .catch((err) => console.log("##### Get Test Error: " + err))
+        .then(() => console.log("************************"))
+
+        .then(() => console.log("**"))
+
+        .then(() => console.log("************************"))
+        .then(() => console.log("Keys Test:"))
+        .then(() => kv.keys())
+        .then((result) => console.log("Stored keys: " + result))
+        .catch((err) => console.log("##### Keys Test Error: " + err))
+        .then(() => console.log("************************"))
+
+        .then(() => console.log("**"))
+
+        .then(() => console.log("************************"))
+        .then(() => console.log("Entries Test:"))
+        .then(() => kv.entries())
+        .then((result) => console.log("Stored entries: " + result))
+        .catch((err) => console.log("##### Entries Test Error: " + err))
+        .then(() => console.log("************************"))
+
+        .then(() => console.log("**"))
+
+        .then(() => console.log("************************"))
+        .then(() => console.log("Delete Test:"))
+        .then(() => kv.entries()).then((result) => console.log(result))
+        .then(() => kv.del('a'))
+        .then(() => kv.entries()).then((result) => console.log(result))
+        .catch((err) => console.log("##### Delete Test Error: " + err))
+        .then(() => console.log("************************"))
+
+        .then(() => console.log("**"))
+
+        .then(() => console.log("************************"))
+        .then(() => console.log("Close Test:"))
+        .then(() => console.log("Connection state : " + kv.con.state))
+        .then(() => kv.close())
+        .then(() => console.log("Connection state : " + kv.con.state))
+        .catch((err) => console.log("##### Close Test Error: " + err))
+        .then(() => console.log("************************"));
 }
