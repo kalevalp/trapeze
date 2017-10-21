2c9d96acfe8e6099cdef19ba781ed093e3b33bc3
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-20 17:33:29 -0700

    Working out some promises code.

--- src/misc/promise-example.js
+++ src/misc/promise-example.js
@@ -6,9 +6,22 @@ const bbPromise = require('bluebird');
 //     .then((val) => console.log("Huhh? " + val + " !!!"))
 //     .then(() => bbPromise.resolve(true))
 //     .then((val) => console.log("Huhh? " + val + " !!!"));
+//
+// bbPromise.resolve()
+//     .then(() => console.log("Test2"))
+//     .then(() => bbPromise.reject("ERROR!"))
+//     .then(() => console.log("Test222"))
+//     .catch((err) => console.log(err));
 
-bbPromise.resolve()
-    .then(() => console.log("Test2"))
-    .then(() => bbPromise.reject("ERROR!"))
-    .then(() => console.log("Test222"))
-    .catch((err) => console.log(err));
+const d1 = bbPromise.resolve("Test");
+console.log(d1);
+
+const func = bbPromise.coroutine(function*(){
+    const d2 = bbPromise.resolve("Test");
+    console.log(d2);
+
+    const d3 = yield bbPromise.resolve("Test");
+    console.log(d3);
+});
+
+func();
