b7830a816022e8426a49925e0397af1c916a64ea
Kalev Alpernas <kalev.alp@gmail.com>
2017-11-04 22:08:50 -0700

    More scratch files.

--- src/misc/promise-example.js
+++ src/misc/promise-example.js
@@ -1,27 +1,43 @@
-const bbPromise = require('bluebird');
-
-// bbPromise.resolve()
-//     .then(() => console.log("Test1"))
-//     .then(() => console.log("test11"))
-//     .then((val) => console.log("Huhh? " + val + " !!!"))
-//     .then(() => bbPromise.resolve(true))
-//     .then((val) => console.log("Huhh? " + val + " !!!"));
+// const bbPromise = require('bluebird');
 //
-// bbPromise.resolve()
-//     .then(() => console.log("Test2"))
-//     .then(() => bbPromise.reject("ERROR!"))
-//     .then(() => console.log("Test222"))
-//     .catch((err) => console.log(err));
+// // bbPromise.resolve()
+// //     .then(() => console.log("Test1"))
+// //     .then(() => console.log("test11"))
+// //     .then((val) => console.log("Huhh? " + val + " !!!"))
+// //     .then(() => bbPromise.resolve(true))
+// //     .then((val) => console.log("Huhh? " + val + " !!!"));
+// //
+// // bbPromise.resolve()
+// //     .then(() => console.log("Test2"))
+// //     .then(() => bbPromise.reject("ERROR!"))
+// //     .then(() => console.log("Test222"))
+// //     .catch((err) => console.log(err));
+//
+// const d1 = bbPromise.resolve("Test");
+// console.log(d1);
+//
+// const func = bbPromise.coroutine(function*(){
+//     const d2 = bbPromise.resolve("Test");
+//     console.log(d2);
+//
+//     const d3 = yield bbPromise.resolve("Test");
+//     console.log(d3);
+// });
+//
+// func();
 
-const d1 = bbPromise.resolve("Test");
-console.log(d1);
+(function loop (i) {
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~");
+    return new Promise(resolve => {
+        console.log(i);
+        resolve();
+        // return i;
+    }).then( () => i >= 10 || loop(i+1) )
+})(0);
 
-const func = bbPromise.coroutine(function*(){
-    const d2 = bbPromise.resolve("Test");
-    console.log(d2);
-
-    const d3 = yield bbPromise.resolve("Test");
-    console.log(d3);
-});
-
-func();
+// (function loop(i) {
+//     if (i <= 10) new Promise(resolve => {
+//         console.log(i);
+//         setTimeout(resolve, Math.random() * 1000);
+//     }).then(loop.bind(null, i+1));
+// })(0);
