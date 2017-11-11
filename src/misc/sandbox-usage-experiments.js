8fe0ae3954a10235425b64e29132c8fe920cfa6e
Kalev Alpernas <kalev.alp@gmail.com>
2017-11-11 13:32:19 -0800

    Playing around with proxies in the sandboxing mechanism.

--- src/misc/sandbox-usage-experiments.js
+++ src/misc/sandbox-usage-experiments.js
@@ -18,6 +18,13 @@
 //   console.log(file);
 // })
 
+const aws = {
+    conf: {
+        a: 10,
+        b: 20
+    },
+}
+
 const {NodeVM} = require('vm2');
 
 // const vm = new NodeVM({
@@ -134,18 +141,33 @@ const {NodeVM} = require('vm2');
 const vmNoConsole = new NodeVM({
     console: 'inherit',
     sandbox: {},
+    // wrapper: 'none',
     require: {
         external: false,
         builtin: [],
         // root: "./",
+        context: 'sandbox',
         mock: {
-            'a' : {
-                A: class {
-                    constructor() {this.b = 10}
-                }
-            },
-            mysql: {
-                connect() { console.log("Nope!"); }
+            // 'a' : {
+            //     A: class {
+            //         constructor() {this.b = 10}
+            //     }
+            // },
+            // mysql: {
+            //     connect() { console.log("Nope!"); }
+            // },
+            'aws' : {
+                conf: aws.conf,
+                // conf: new Proxy(aws.conf, {
+                //     set(target, property, value, receiver) {
+                //         console.log(target);
+                //         console.log(property);
+                //         console.log(value);
+                //         console.log(receiver);
+                //     }
+                // }),
+
+                addc: () => {aws.conf.c = 100},
             }
         }
     }
@@ -159,7 +181,20 @@ const vmNoConsole = new NodeVM({
 //     const a = new A();
 //     console.log(a.b);
 // `);
+// vmNoConsole.run(`
+//     const p = Promise.resolve(10101010);
+//     p.then(ten => console.log(ten));
+// `);
+
 vmNoConsole.run(`
-    const p = Promise.resolve(10101010);
-    p.then(ten => console.log(ten));
+    'use strict';
+    
+    const aws = require('aws');
+    console.log(aws);
+    // aws.c = 111;
+    // console.log(aws.c);
+    aws.conf.c = 100;
+    console.log(aws);
+    // aws.addc();
+    // console.log(aws);
 `);
