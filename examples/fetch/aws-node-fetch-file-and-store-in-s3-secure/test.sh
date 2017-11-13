87f849d40030fda539f2c5afa41c9488010e037e
Kalev Alpernas <kalev.alp@gmail.com>
2017-11-12 21:21:33 -0800

    Fetch test scenario

--- examples/fetch/aws-node-fetch-file-and-store-in-s3-secure/test.sh
+++ examples/fetch/aws-node-fetch-file-and-store-in-s3-secure/test.sh
@@ -1,3 +1,42 @@
 #!/usr/bin/env bash
 
-curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat1", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat2", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat3", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
+time curl -X POST --data '{"key": "cat4", "image_url" : "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "user" : "catowner", "pass" : "catownerpassword"}' $1
+time curl -X POST --data '{"key": "cat5", "image_url" : "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg", "user" : "dogowner", "pass" : "dogownerpassword"}' $1
