0217e01e7de02386c626435d2d99606903244428
Kalev Alpernas <kalev.alp@gmail.com>
2018-04-14 22:39:01 +0300

    Examples to demonstrate the effect of the fork-optimization on the overhead.

---
+++ examples/overhead-sim/ninety/safe-opt/handler.js
@@ -0,0 +1,24 @@
+'use strict';
+const aws = require('aws-sdk');
+const AJV = require('ajv');
+const fs = require('fs');
+const Promise = require('bluebird');
+const KH = require('kinesis-handler');
+const nodemailer = require('nodemailer');
+const got = require('got');
+const url = require('url');
+
+const productPurchaseSchema = require('./schemas/product-purchase-schema.json');
+const productCreateSchema = require('./schemas/product-create-schema.json');
+const userLoginSchema = require('./schemas/user-login-schema.json');
+const updatePhoneSchema = require('./schemas/user-update-phone-schema.json');
+const addRoleSchema = require('./schemas/user-add-role-schema.json');
+
+console.log('### Finished all requires in handler.js');
+
+module.exports.hello = (event, context, callback) => {
+    console.log('### Call to hello in handler.js');
+    setTimeout(function() {
+        callback(null, { message: 'Success!'});
+    }, 125);
+};
---
+++ examples/overhead-sim/ninety/safe/secure-handler.js
@@ -0,0 +1,3 @@
+const shim = require('lambda-shim');
+
+shim.makeShim(module.exports, true);
